2620d61536eb0da54be6754224813294ce6eb824
Arturo Javier-Castellanos <arturo@karveit.ca>
2026-02-24 10:59:46 -0600

    Add automation investments

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { AutomationInvestments } from './automation-investments';
 
 @Component({
     selector: 'app-home',
+    standalone: true,
+    imports: [AutomationInvestments],
     template: `
         <div class="grid grid-cols-12 gap-8">
+            <div class="col-span-12">
+                <app-automation-investments />
+            </div>
         </div>
     `
 })
