5acd1cc9bfa673ee0f64658d0a49e21cf668de5a
Marcelo <22820046+marcelosmc@users.noreply.github.com>
2026-02-24 11:41:35 -0600

    Tied the calculate service to the front end

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { AutomationInvestments } from './automation-investments';
+import { MvpRoiCalculator } from './mvp-roi-calculator';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [AutomationInvestments],
+    imports: [AutomationInvestments, MvpRoiCalculator],
     template: `
         <div class="grid grid-cols-12 gap-8">
             <div class="col-span-12">
                 <app-automation-investments />
+            </div>
+            <div class="col-span-12">
+                <app-mvp-roi-calculator />
             </div>
         </div>
     `
