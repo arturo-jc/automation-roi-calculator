7828d01490ef2b70c0ffd8a41fc5287b9f5aae56
Arturo Javier-Castellanos <a.javier.castellanos@gmail.com>
2026-02-24 12:45:05 -0600

    Fix layout

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { AutomationInvestments } from './automation-investments';
-import { MvpRoiCalculator } from './mvp-roi-calculator';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [AutomationInvestments, MvpRoiCalculator],
+    imports: [AutomationInvestments],
     template: `
-        <div class="grid grid-cols-12 gap-8">
+        <div class="max-w-7xl mx-auto grid grid-cols-12 gap-8">
             <div class="col-span-12">
                 <app-automation-investments />
-            </div>
-            <div class="col-span-12">
-                <app-mvp-roi-calculator />
             </div>
         </div>
     `
